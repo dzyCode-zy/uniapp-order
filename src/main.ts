68e7f2bc98727c860cb7b521e05060db0e6f8f22
daizy <dziyu47@gmail.com>
2025-01-13 17:57:59 +0800

    test name

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createSSRApp } from 'vue'
 import pinia from './stores'
-import './utils/http.ts'
+import './utils/http.ts' // 全局加载，此后的页面都不用引入了
 
 import App from './App.vue'
 export function createApp() {
